2f9c3ed852add55fcec805fd51d646ba49115160
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-16 22:42:29 +0900

    :lipstick: Style: Login 페이지 레이아웃 구성
    Login 페이지 레이아웃 구성

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,39 @@
+import Button from '../components/common/Button/Button';
+import InputField from '../components/common/Input/InputField';
+import { Heading2 } from '../components/common/Heading/HeadingStyles';
+import {
+  GridLayout,
+  LoginForm,
+  LoginSideBox,
+  SignupText,
+  SignupLink,
+} from '../components/Login/LoginStyles';
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <GridLayout>
+      <LoginSideBox bg />
+      <LoginSideBox>
+        <Heading2 center>회원 계정으로 로그인하세요!</Heading2>
+        <LoginForm>
+          <InputField
+            element="input"
+            label="Email"
+            type="email"
+            placeholder="example@example.com"
+          />
+          <InputField element="input" label="Password" type="password" />
+          <Button padding="lg" primary>
+            로그인
+          </Button>
+        </LoginForm>
+        <SignupText>
+          처음 방문하셨나요?
+          <SignupLink to="/signup">회원가입</SignupLink>
+        </SignupText>
+      </LoginSideBox>
+    </GridLayout>
+  );
 };
 
 export default Login;
