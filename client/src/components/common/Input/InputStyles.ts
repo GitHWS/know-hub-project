42b658c816f1d50c4ed4f0ffaa20407b3eaedb88
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-16 14:02:05 +0900

    :recycle: Modify: 모든 스타일 컴포넌트명 변경 및 일부 스타일 수정
    모든 Input 관련 스타일 컴포넌트명 변경 및 일부 스타일 수정

--- client/src/components/common/Input/InputStyles.ts
+++ client/src/components/common/Input/InputStyles.ts
@@ -2,31 +2,32 @@ import styled, { css } from 'styled-components';
 
 import { theme } from '../../../styles/theme';
 
-export const InputFieldStyle = styled.div`
+export const InputBox = styled.div`
   display: flex;
   flex-direction: column;
   gap: 5px;
 `;
 
-export const InputLabelStyle = styled.label`
+export const Label = styled.label`
   font-size: 2rem;
   color: ${theme.colors.white};
 `;
 
 const commonInputStyle = css`
-  color: ${theme.colors.white};
+  width: 100%;
   border: 2px solid ${theme.colors.lightGrey};
   border-radius: 10px;
-  background-color: ${theme.colors.grey};
   font-size: 2rem;
+  color: ${theme.colors.white};
+  background-color: ${theme.colors.grey};
 `;
 
-export const InputStyle = styled.input`
+export const Input = styled.input`
   ${commonInputStyle}
   padding: 20px 10px;
 `;
 
-export const TextareaStyle = styled.textarea`
+export const Textarea = styled.textarea`
   ${commonInputStyle}
   padding: 15px;
 `;
