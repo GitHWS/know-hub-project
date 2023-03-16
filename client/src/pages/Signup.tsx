1c3784de7fb05a55d0dc5f9200590c1ab2746271
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-16 22:43:51 +0900

    :lipstick: Style: Signup 페이지 레이아웃 구성
    Signup 페이지 레이아웃 구성(임시 회원가입 단계 기능)

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+import Button from '../components/common/Button/Button';
+import { Heading2 } from '../components/common/Heading/HeadingStyles';
+import InputField from '../components/common/Input/InputField';
+import {
+  GridLayout,
+  LoginForm,
+  LoginSideBox,
+  SignupLink,
+  SignupText,
+} from '../components/Login/LoginStyles';
+
 const Signup = () => {
-  return <div>Signup</div>;
+  const [signupStep, setSignupStep] = useState(3);
+
+  return (
+    <GridLayout>
+      <LoginSideBox bg />
+      <LoginSideBox>
+        <Heading2 center>회원가입</Heading2>
+        <LoginForm>
+          {signupStep === 1 && (
+            <>
+              <InputField
+                element="input"
+                label="Email"
+                type="email"
+                placeholder="example@example.com"
+              />
+              <InputField element="input" label="Password" type="password" />
+              <Button padding="lg" primary>
+                다음
+              </Button>
+            </>
+          )}
+          {signupStep === 2 && (
+            <>
+              <InputField element="input" label="Name" type="text" />
+              <InputField element="textarea" label="Bio" />
+              <Button padding="lg" primary>
+                다음
+              </Button>
+            </>
+          )}
+          {signupStep === 3 && (
+            <>
+              <InputField element="input" label="Profile Image" type="file" />
+              <Button padding="lg" primary>
+                회원가입
+              </Button>
+            </>
+          )}
+        </LoginForm>
+      </LoginSideBox>
+    </GridLayout>
+  );
 };
 
 export default Signup;
