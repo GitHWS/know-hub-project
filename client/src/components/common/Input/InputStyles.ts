bb07a67dcc30fb78471725c46c1ee057b71008cc
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-16 22:21:09 +0900

    :lipstick: Style: Input 관련 컴포넌트 일부 스타일 수정
    Input 관련 컴포넌트 일부 스타일 수정

--- client/src/components/common/Input/InputStyles.ts
+++ client/src/components/common/Input/InputStyles.ts
@@ -9,25 +9,25 @@ export const InputBox = styled.div`
 `;
 
 export const Label = styled.label`
-  font-size: 2rem;
-  color: ${theme.colors.white};
+  font-size: 1.5rem;
+  color: ${theme.colors.lightGrey};
 `;
 
 const commonInputStyle = css`
   width: 100%;
   border: 2px solid ${theme.colors.lightGrey};
   border-radius: 10px;
-  font-size: 2rem;
+  font-size: 1.8rem;
   color: ${theme.colors.white};
   background-color: ${theme.colors.grey};
 `;
 
 export const Input = styled.input`
   ${commonInputStyle}
-  padding: 20px 10px;
+  padding: 10px 5px;
 `;
 
 export const Textarea = styled.textarea`
   ${commonInputStyle}
-  padding: 15px;
+  padding: 10px;
 `;
