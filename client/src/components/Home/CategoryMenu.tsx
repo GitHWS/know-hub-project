07d1165771c8dbc793b17b3128d0e7781b24761d
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-17 23:47:31 +0900

    :lipstick: Style: CategoryMenu 스타일 컴포넌트 생성 및 CategoryMenu 컴포넌트 레이아웃 구성
    CategoryMenu 스타일 컴포넌트 생성 및 CategoryMenu 컴포넌트 레이아웃 구성

---
+++ client/src/components/Home/CategoryMenu.tsx
@@ -0,0 +1,25 @@
+import { FlexBox } from '../common/Align/AlignStyle';
+import { Heading4 } from '../common/Heading/HeadingStyles';
+import { CategoryMenuList, CreateCategoryButton } from './CategoryMenuStyles';
+import TabButton from '../common/Button/TabButton';
+import { SecondaryButton } from '../common/Button/ButtonStyles';
+
+const CategoryMenu = () => {
+  return (
+    <FlexBox col gap={10}>
+      <FlexBox between>
+        <Heading4>카테고리</Heading4>
+        <CreateCategoryButton />
+      </FlexBox>
+      <CategoryMenuList>
+        <TabButton title="react">React</TabButton>
+        <TabButton title="vue">Vue</TabButton>
+      </CategoryMenuList>
+      <div>
+        <SecondaryButton size="lg">로그아웃</SecondaryButton>
+      </div>
+    </FlexBox>
+  );
+};
+
+export default CategoryMenu;
---
+++ client/src/components/Home/CategoryMenuStyles.ts
@@ -0,0 +1,37 @@
+import styled, { keyframes } from 'styled-components';
+import iconCreate from '../../assets/images/icon_create.svg';
+
+const rotateAni = keyframes`
+  from{
+    transform: rotate(0);
+  }
+  to{
+    transform: rotate(45deg);
+  }
+`;
+
+export const CategoryMenuList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const CreateCategoryButton = styled.button`
+  width: 24px;
+  height: 24px;
+  transition: transform 0.1s ease-in-out;
+
+  &::before {
+    display: inline-block;
+    content: '';
+    width: 100%;
+    height: 100%;
+    background-image: url(${iconCreate});
+  }
+
+  &:hover {
+    &::before {
+      animation: ${rotateAni} 0.1s ease-in-out forwards;
+    }
+  }
+`;
