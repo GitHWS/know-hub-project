2408c01c5e78f46344eb910392ee524abff2ec88
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-17 23:46:07 +0900

    :lipstick: Style: UserProfile 스타일 컴포넌트 및 레이아웃 설정
    UserProfile 스타일 컴포넌트 생성 및 UserProfile 컴포넌트 레이아웃 설정

---
+++ client/src/components/Home/UserProfile.tsx
@@ -0,0 +1,31 @@
+import { FlexBox } from '../common/Align/AlignStyle';
+import { PrimaryButton } from '../common/Button/ButtonStyles';
+import {
+  ProfileBio,
+  ProfileImage,
+  ProfileJob,
+  ProfileName,
+} from './UserProfileStyles';
+
+const UserProfile = () => {
+  return (
+    <FlexBox col center gap={15}>
+      <div>
+        <ProfileImage
+          src="https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+          alt="사용자 프로필 이미지"
+        />
+      </div>
+      <FlexBox col center gap={5}>
+        <ProfileName>사용자</ProfileName>
+        <ProfileJob>직업</ProfileJob>
+      </FlexBox>
+      <ProfileBio>
+        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit.
+      </ProfileBio>
+      <PrimaryButton size="sm">프로필 수정</PrimaryButton>
+    </FlexBox>
+  );
+};
+
+export default UserProfile;
---
+++ client/src/components/Home/UserProfileStyles.ts
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { theme } from '../../styles/theme';
+
+export const ProfileImage = styled.img`
+  width: 20rem;
+  height: 20rem;
+  border-radius: 50%;
+`;
+
+export const ProfileName = styled.span`
+  color: ${theme.colors.white};
+  font-size: 1.8rem;
+  font-weight: 900;
+`;
+
+export const ProfileJob = styled.span`
+  color: ${theme.colors.lightGrey};
+  font-size: 1.2rem;
+`;
+
+export const ProfileBio = styled.p`
+  color: ${theme.colors.white};
+  font-size: 1.4rem;
+  text-align: center;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+`;
