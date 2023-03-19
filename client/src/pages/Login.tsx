16ec2657e79de292b8f9cbb86f2958626534cd79
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-19 22:10:56 +0900

    :recycle: Modify: Login, Signup 페이지 일부 컴포넌트 수정으로 경로 수정
    Login, Signup 페이지 일부 컴포넌트 수정으로 경로 수정

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,36 +1,40 @@
 import InputField from '../components/common/Input/InputField';
+
+import { FlexAlignCenter } from '../components/common/Layout/LayoutStyles';
+import { CenterGridLayout } from '../components/common/Layout/LayoutStyles';
 import { Heading2 } from '../components/common/Heading/HeadingStyles';
 import { PrimaryButton } from '../components/common/Button/ButtonStyles';
 import {
-  GridLayout,
-  LoginForm,
-  LoginSideBox,
+  Form,
+  InputWrapBox,
   SignupText,
   SignupLink,
-} from '../components/Login/LoginStyles';
+} from '../components/Form/FormStyles';
 
 const Login = () => {
   return (
-    <GridLayout>
-      <LoginSideBox bg />
-      <LoginSideBox>
-        <Heading2 center>회원 계정으로 로그인하세요!</Heading2>
-        <LoginForm>
-          <InputField
-            element="input"
-            label="Email"
-            type="email"
-            placeholder="example@example.com"
-          />
-          <InputField element="input" label="Password" type="password" />
-          <PrimaryButton size="lg">로그인</PrimaryButton>
-        </LoginForm>
-        <SignupText>
-          처음 방문하셨나요?
-          <SignupLink to="/signup">회원가입</SignupLink>
-        </SignupText>
-      </LoginSideBox>
-    </GridLayout>
+    <CenterGridLayout>
+      <FlexAlignCenter>
+        <InputWrapBox bg="image" />
+        <InputWrapBox col gap={40}>
+          <Heading2 center>회원 계정으로 로그인하세요!</Heading2>
+          <Form>
+            <InputField
+              element="input"
+              label="Email"
+              type="email"
+              placeholder="example@example.com"
+            />
+            <InputField element="input" label="Password" type="password" />
+            <PrimaryButton size="lg">로그인</PrimaryButton>
+          </Form>
+          <SignupText>
+            처음 방문하셨나요?
+            <SignupLink to="/signup">회원가입</SignupLink>
+          </SignupText>
+        </InputWrapBox>
+      </FlexAlignCenter>
+    </CenterGridLayout>
   );
 };
 
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,53 +1,52 @@
 import { useState } from 'react';
 
 import InputField from '../components/common/Input/InputField';
-import { PrimaryButton } from '../components/common/Button/ButtonStyles';
+
+import { CenterGridLayout } from '../components/common/Layout/LayoutStyles';
+import { FlexAlignCenter } from '../components/common/Layout/LayoutStyles';
 import { Heading2 } from '../components/common/Heading/HeadingStyles';
-import {
-  GridLayout,
-  LoginForm,
-  LoginSideBox,
-  SignupLink,
-  SignupText,
-} from '../components/Login/LoginStyles';
+import { PrimaryButton } from '../components/common/Button/ButtonStyles';
+import { Form, InputWrapBox } from '../components/Form/FormStyles';
 
 const Signup = () => {
-  const [signupStep, setSignupStep] = useState(3);
+  const [signupStep, setSignupStep] = useState(1);
 
   return (
-    <GridLayout>
-      <LoginSideBox bg />
-      <LoginSideBox>
-        <Heading2 center>회원가입</Heading2>
-        <LoginForm>
-          {signupStep === 1 && (
-            <>
-              <InputField
-                element="input"
-                label="Email"
-                type="email"
-                placeholder="example@example.com"
-              />
-              <InputField element="input" label="Password" type="password" />
-              <PrimaryButton size="lg">다음</PrimaryButton>
-            </>
-          )}
-          {signupStep === 2 && (
-            <>
-              <InputField element="input" label="Name" type="text" />
-              <InputField element="textarea" label="Bio" />
-              <PrimaryButton size="lg">다음</PrimaryButton>
-            </>
-          )}
-          {signupStep === 3 && (
-            <>
-              <InputField element="input" label="Profile Image" type="file" />
-              <PrimaryButton size="lg">회원가입</PrimaryButton>
-            </>
-          )}
-        </LoginForm>
-      </LoginSideBox>
-    </GridLayout>
+    <CenterGridLayout>
+      <FlexAlignCenter>
+        <InputWrapBox bg="image" />
+        <InputWrapBox col gap={40}>
+          <Heading2 center>회원가입</Heading2>
+          <Form>
+            {signupStep === 1 && (
+              <>
+                <InputField
+                  element="input"
+                  label="Email"
+                  type="email"
+                  placeholder="example@example.com"
+                />
+                <InputField element="input" label="Password" type="password" />
+                <PrimaryButton size="lg">다음</PrimaryButton>
+              </>
+            )}
+            {signupStep === 2 && (
+              <>
+                <InputField element="input" label="Name" type="text" />
+                <InputField element="textarea" label="Bio" />
+                <PrimaryButton size="lg">다음</PrimaryButton>
+              </>
+            )}
+            {signupStep === 3 && (
+              <>
+                <InputField element="input" label="Profile Image" type="file" />
+                <PrimaryButton size="lg">회원가입</PrimaryButton>
+              </>
+            )}
+          </Form>
+        </InputWrapBox>
+      </FlexAlignCenter>
+    </CenterGridLayout>
   );
 };
 
