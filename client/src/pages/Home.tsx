8fec20ba0aa668e043336de21f079fc2ce64336a
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-03-19 22:13:43 +0900

    :lipstick: Style: Home 페이지 레이아웃 구성
    Home 페이지 레이아웃 구성

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import PostList from '../components/Home/PostList/PostList';
+import AccountSideBar from '../components/Home/AccountSideBar';
+
+import { MainGridLayout } from '../components/common/Layout/LayoutStyles';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <MainGridLayout>
+      <AccountSideBar />
+      <div>Editor</div>
+      <PostList />
+    </MainGridLayout>
+  );
 };
 
 export default Home;
