3add73e7f60ca5d9af53b6acfab0a25053e35a27
Hyunwoo Shin <96808980+GitHWS@users.noreply.github.com>
2023-04-04 17:47:32 +0900

    :recycle: Modify: 테마 변경 및 기본 폰트 스타일 추가로 변경사항 적용
    테마 변경 및 기본 폰트 스타일 추가로 변경사항 적용

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyle from './styles/GlobalStyles';
-import { theme } from './styles/theme';
+import theme from './styles/theme';
+import './assets/fonts/fonts.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ThemeProvider theme={theme}>
